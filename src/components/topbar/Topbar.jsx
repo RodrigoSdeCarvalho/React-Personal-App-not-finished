dc90c01b9773ae51c6cacd91ad26ee4dcb7e6b50
Rodrigo Santos de Carvalho <rodrigosc2401@gmail.com>
2022-03-27 11:01:15 -0300

    added copy email and phone number functionality

--- src/components/topbar/Topbar.jsx
+++ src/components/topbar/Topbar.jsx
@@ -1,6 +1,9 @@
 import './topbar.scss'
 import {Person, Mail} from "@material-ui/icons"
 
+const phone_number = "+55 (47) 99696-0576"
+const email = "rodrigosc2401@gmail.com"
+
 export default function Topbar() {
   return (
     <div className="topbar">
@@ -9,14 +12,27 @@ export default function Topbar() {
           <a href="#intro" className="logo">Rodrigo</a>
           <div className="itemContainer">
             <Person className="icon"/>
-            <span>+47 99696-0576</span>
+            <button onClick={() => {
+              navigator.clipboard.writeText(phone_number);
+              alert('Phone number copied to clipboard');
+              }}><p className="text">{phone_number}</p>
+            </button>
           </div>
           <div className="itemContainer">
             <Mail className="icon"/>
-            <span>rodrigosc2401@gmail.com</span>
+            <button onClick={() => {
+              navigator.clipboard.writeText(email);
+              alert('E-mail copied to clipboard');
+              }}><p className="text">{email}</p>
+            </button>
           </div>
         </div>
         <div className="right">
+          <div className="hamburger">
+            <span className="line1"></span>
+            <span className="line2"></span>
+            <span className="line3"></span>
+          </div>
         </div>
       </div>
     </div>
