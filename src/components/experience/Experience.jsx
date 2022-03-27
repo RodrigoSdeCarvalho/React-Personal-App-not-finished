499a35603ecc94f79b723f9497e312b9de17b317
Rodrigo Santos de Carvalho <rodrigosc2401@gmail.com>
2022-03-27 09:45:56 -0300

    Navbar menu structured

--- src/components/experience/Experience.jsx
+++ src/components/experience/Experience.jsx
@@ -2,7 +2,7 @@ import "./experience.scss"
 
 export default function Experience() {
   return (
-    <div className="experience">
+    <div className="experience" id="experience">
       
     </div>
   )
--- src/components/topbar/Topbar.jsx
+++ src/components/topbar/Topbar.jsx
@@ -2,7 +2,13 @@ import './topbar.scss'
 export default function Topbar() {
   return (
     <div className="topbar">
-      
+      <div className="wrapper">
+        <div className="left">
+          <a href="#intro" className="logo">About me</a>
+        </div>
+        <div className="right">
+        </div>
+      </div>
     </div>
   )
 }
