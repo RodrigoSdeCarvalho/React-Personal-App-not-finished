0fe6f2e13371cd1a9ff53410adb7ebafbd8115b4
Rodrigo Santos de Carvalho <rodrigosc2401@gmail.com>
2022-03-27 10:08:12 -0300

    Icons added to topbar

--- src/components/topbar/Topbar.jsx
+++ src/components/topbar/Topbar.jsx
@@ -1,10 +1,20 @@
 import './topbar.scss'
+import {Person, Mail} from "@material-ui/icons"
+
 export default function Topbar() {
   return (
     <div className="topbar">
       <div className="wrapper">
         <div className="left">
-          <a href="#intro" className="logo">About me</a>
+          <a href="#intro" className="logo">Rodrigo</a>
+          <div className="itemContainer">
+            <Person className="icon"/>
+            <span>+47 99696-0576</span>
+          </div>
+          <div className="itemContainer">
+            <Mail className="icon"/>
+            <span>rodrigosc2401@gmail.com</span>
+          </div>
         </div>
         <div className="right">
         </div>
