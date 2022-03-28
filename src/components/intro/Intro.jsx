2ce855a82e320bd70ce2e0d4a702e0e386d80ffb
Rodrigo Santos de Carvalho <rodrigosc2401@gmail.com>
2022-03-28 02:38:21 -0300

    Intro page

--- src/components/intro/Intro.jsx
+++ src/components/intro/Intro.jsx
@@ -1,9 +1,35 @@
-import './intro.scss'
+import "./intro.scss";
+import { init } from "ityped";
+import { useEffect, useRef } from "react";
 
 export default function Intro() {
+  const textRef = useRef();
+
+  useEffect(() => {
+    init(textRef.current, {
+      showCursor: true,
+      backDelay: 1500,
+      backSpeed:60,
+      strings: ["Machine Learning Researcher", "Developer", "Computer Science Student"],
+    });
+  }, []);
+
   return (
     <div className="intro" id="intro">
-      
+      <div className="center">
+        <div className="wrapper">
+          <h2>Hi, I'm</h2>
+          <h1> Rodrigo de Carvalho,  </h1>
+          <h3>
+          a <span ref={textRef}></span>
+          </h3>
+        </div>
+      </div>
+      <div className="arrow">
+        <a href="#portfolio">
+          <img src="assets/down.png" alt="" />
+        </a>
+      </div>
     </div>
-  )
+  );
 }
