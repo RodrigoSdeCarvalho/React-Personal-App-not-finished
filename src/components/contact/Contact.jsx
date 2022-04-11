b57f9374446a6458611ca5a470775514c6fc9c93
Rodrigo Santos de Carvalho <rodrigosc2401@gmail.com>
2022-04-11 14:57:31 -0300

    Contact page

--- src/components/contact/Contact.jsx
+++ src/components/contact/Contact.jsx
@@ -1,9 +1,39 @@
+import { useState } from "react";
 import "./contact.scss"
 
 export default function Contact() {
+
+  const [message, setMessage] = useState(false)
+
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    setMessage(true)
+  }
+
   return (
     <div className='contact' id="contact">
-      
+      <div className="left">
+        <img src="assets/shake.svg" alt=""/>
+      </div>
+      <div className="right">
+        <h2>
+          Contact.
+        </h2>
+        <form onSubmit={handleSubmit}>
+          <input type="text" placeholder="Email"/>
+          <textarea placeholder="Message"></textarea>
+          <button type="submit">Send</button>
+          {message && <span>Thanks, I'll reply ASAP!</span>}
+        </form>
+        <div className="socialMediaContainer">
+        <a href="https://www.linkedin.com/in/rodrigo-santos-de-carvalho/" target="_blank"><div className="github">
+              <img src="assets/linkedIn.png" alt=""/>
+            </div></a>
+            <a href="https://github.com/RodrigoSdeCarvalho/" target="_blank"><div className="github">
+              <img src="assets/github.png" alt=""/>
+            </div></a>
+          </div>
+      </div>
     </div>
   )
 }
